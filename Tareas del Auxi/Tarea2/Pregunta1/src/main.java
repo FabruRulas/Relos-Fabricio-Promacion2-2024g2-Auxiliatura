4348b11e22d1be708a01f017b30b246eeba62816
FabruRulas <fabriciorelos@gmail.com>
2024-09-20 15:49:28 -0400

    Mejorando Ej1

--- Tareas del Auxi/Tarea2/Pregunta1/src/main.java
+++ Tareas del Auxi/Tarea2/Pregunta1/src/main.java
@@ -1,94 +1,46 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String Texto = scanner.nextLine();
-		String guardar[];
-		
-		guardar= Texto.split(" ");
-		
-		
-		switch(guardar[2]) {
-			case "enero,":
-				if(31 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "01";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "febrero,":
-				if(29 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "02";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "marzo,":
-				if(31 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "03";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "abril,":
-				if(30 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "04";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "mayo,":
-				if(31 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "05";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "junio,":
-				if(30 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "06";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "julio,":
-				if(31 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "07";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "agosto,":
-				if(31 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "08";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "septiembre,":
-				if(30 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "09";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "octubre,":
-				if(31 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "10";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "noviembre,":
-				if(30 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "11";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			case "diciembre,":
-				if(31 >= Integer.parseInt(guardar[0])) {
-					guardar[2] = "12";
-					System.out.println(guardar[0]+"/"+guardar[2]+"/"+guardar[3]);
-				}
-			break;
-			default:
-				System.out.println("ERROR ERROR ERROR");
-			break;
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+		String fecha = scanner.nextLine();
+		String fechasCortadas[];
+		String meses[] = {" ","enero,","febrero,","marzo,","abril,","mayo,","junio,","julio,",
+				"agosto,","septiembre,","octubre,","noviembre,","diciembre,"};
+		fechasCortadas = fecha.split(" ");
+		int mes = 0;
+		for(int i=0; i<meses.length; i++) {
+			if(meses[i].equals(fechasCortadas[2])) {
+				mes = i;
+			}
 		}
-				
 
+		if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
+			if(Integer.valueOf(fechasCortadas[0])<=31) {
+				if(mes<10)
+					System.out.println(fechasCortadas[0]+"/0"+mes+"/"+fechasCortadas[3]);
+				else
+					System.out.println(fechasCortadas[0]+"/"+mes+"/"+fechasCortadas[3]);
+			}
+		}
+		
+		if(mes== 4 || mes == 6 || mes == 9 || mes == 11) {
+			if(Integer.valueOf(fechasCortadas[0])<=30) {
+				if(mes<10)
+					System.out.println(fechasCortadas[0]+"/0"+mes+"/"+fechasCortadas[3]);
+				else
+					System.out.println(fechasCortadas[0]+"/"+mes+"/"+fechasCortadas[3]);
+			}
+		}
+		
+		if(mes == 2) {
+			if(Integer.valueOf(fechasCortadas[0])<=29) {
+				System.out.println(fechasCortadas[0]+"/0"+mes+"/"+fechasCortadas[3]);
+			}
+		}
 	}
 
 }
